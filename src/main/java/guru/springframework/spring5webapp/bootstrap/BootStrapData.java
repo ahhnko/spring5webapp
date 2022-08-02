52d1a14681fbb6fadcba331721c4f698a67547e2
Andrés Halliday Martínez <ahalliday@asesoftware.com>
2022-08-01 22:58:49 -0500

    .

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -5,41 +5,66 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
 
 	private final AuthorRepository authorRepository;
 	private final BookRepository bookRepository;
+	private final PublisherRepository publisherRepository;
 
-	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+			PublisherRepository publisherRepository) {
 		super();
 		this.authorRepository = authorRepository;
 		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		Publisher elPubli = new Publisher();
+		elPubli.setName("elpi");
+		elPubli.setAddressLine1("asf");
+		elPubli.setCity("bo");
+		elPubli.setState("cun");
+		elPubli.setZip("asfd421");
+
+		publisherRepository.save(elPubli);
+
+		System.out.println("Number of publishers: " + publisherRepository.count());
+
 		Author eric = new Author("Eric", "Evans");
 		Book ddd = new Book("Domain Driven Design", "123123");
 		eric.getBooks().add(ddd);
 		ddd.getAuthors().add(eric);
 
+		ddd.setPublisher(elPubli);
+		elPubli.getBooks().add(ddd);
+
 		authorRepository.save(eric);
 		bookRepository.save(ddd);
+		publisherRepository.save(elPubli);
 
 		Author rod = new Author("Rod", "Johnson");
 		Book noEJB = new Book("J2EE Development without EJB", "23452355");
 		rod.getBooks().add(noEJB);
 		noEJB.getAuthors().add(rod);
 
+		noEJB.setPublisher(elPubli);
+		elPubli.getBooks().add(noEJB);
+
 		authorRepository.save(rod);
 		bookRepository.save(noEJB);
+		publisherRepository.save(elPubli);
 
 		System.out.println("Started in Bootstrap");
 		System.out.println("Number of Books: " + bookRepository.count());
+		System.out.println("Publisher Number of books: " + elPubli.getBooks().size());
 	}
 
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -11,6 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Book {
@@ -19,6 +20,8 @@ public class Book {
 	private Long id;
 	private String title;
 	private String isbn;
+	@ManyToOne
+	private Publisher publisher;
 	@ManyToMany
 	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
 	private Set<Author> authors = new HashSet<>();
@@ -63,6 +66,14 @@ public class Book {
 		this.isbn = isbn;
 	}
 
+	public Publisher getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -82,6 +93,6 @@ public class Book {
 
 	@Override
 	public String toString() {
-		return "Book [id=" + id + ", title=" + title + ", isbn=" + isbn + ", author=" + authors + "]";
+		return "Book [id=" + id + ", title=" + title + ", isbn=" + isbn + "]";
 	}
 }
